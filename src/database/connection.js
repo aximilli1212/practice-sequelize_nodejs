975a50036876c48b828cfdfe6de0f8a066d593d5
aximilli1212 <aximilli1212@gmail.com>
2019-11-24 10:07:09 +0000

    Finalize Project

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 const sequelize = new Sequelize("socialnewtwork", 'root','root', {host:'akoko_mysql_1', dialect:'mysql', operatorAliases:false})
 
-modules.exports = sequelize;
+module.exports = sequelize;
 global.sequelize = sequelize;
 
