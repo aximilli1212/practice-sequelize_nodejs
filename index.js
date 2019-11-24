f88c11422ad41e09f418345894d6c9cd182ee738
aximilli1212 <aximilli1212@gmail.com>
2019-11-24 07:09:44 +0000

    Setting up Sequelize and CLI

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+let app = express();
+const PORT = 3000;
+
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.set("view engine", "pug");
+app.set("views", path.resolve("./src/views"));
+
+//Request Parsing Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
+//Create Express Router
+const router = express.Router();
+app.use(router);
+
+const rootPath = path.resolve("./dist");
+app.use(express.static(rootPath));
