5e7d59dcb61e4f95ab6e62edc10b016cbe8e59de
aximilli1212 <aximilli1212@gmail.com>
2019-11-24 08:18:36 +0000

    Added seeder@bootstrap for Tweets

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,5 +6,9 @@ module.exports = async () => {
         console.log("Error: ", err);
     }
     const user = await User.create({username:"Alex Baldwin", password:"12345"}).catch(errHandler);
+    const tweet = await Tweet.create({content: "This is a new tweet with the sound of heaven.", userId: user.id}).catch(errHandler)
+
+
+
 
 }
